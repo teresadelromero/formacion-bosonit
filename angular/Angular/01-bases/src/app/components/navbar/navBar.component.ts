46e159c703425632399f05705f5920d5f3efcdc7
tdelromerom <tdelromero@birt.eus>
2022-10-28 14:50:18 +0200

    Header, Footer Components Create

---
+++ angular/Angular/01-bases/src/app/components/navbar/navBar.component.ts
@@ -0,0 +1,11 @@
+import {  Component } from '@angular/core';
+
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navBar.component.html',
+    styleUrls: ['./navBar.component.css']
+})
+
+export class NavBarComponent{
+
+}
