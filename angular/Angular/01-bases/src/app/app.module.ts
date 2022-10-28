e9020b9840d72f4c113eefdfc8eb4d1e2dbf59fe
tdelromerom <tdelromero@birt.eus>
2022-10-28 14:40:53 +0200

    Header, Footer Components Create

--- angular/Angular/01-bases/src/app/app.module.ts
+++ angular/Angular/01-bases/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-
-
+import {NavBarComponent} from './components/navbar/navBar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import {LandigPageHeaderComponent} from './components/landigPageHeader/landigPageHeader.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    LandigPageHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular/Angular/01-bases/src/app/components/landigPageHeader/landigPageHeader.component.ts
@@ -0,0 +1,11 @@
+import {  Component } from '@angular/core';
+
+@Component({
+    selector: 'app-landing-page-header',
+    templateUrl: './landigPageHeader.component.html',
+    styleUrls: ['./landigPageHeader.component.css']
+})
+
+export class LandigPageHeaderComponent{
+
+}
